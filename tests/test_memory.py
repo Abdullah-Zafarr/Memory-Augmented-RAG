179c6ed0fc708bb504fc0d3e72042e7ce6b7bf72
abdullahzafar08 <857895.r@gmail.com>
2026-06-21 15:00:00 +0500

    Test: Add memory retrieval test verifying returns empty list for new users

---
+++ tests/test_memory.py
@@ -0,0 +1,7 @@
+import pytest
+import memory
+
+def test_memory_retrieval_empty():
+    mems = memory.retrieve_memory("non_existent_user_xyz", "hello")
+    assert isinstance(mems, list)
+    assert len(mems) == 0
